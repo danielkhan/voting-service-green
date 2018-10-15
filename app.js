e0897083a786b3980414f8c8c8b02c991ec5164d
Daniel Khan <daniel.khan@dynatrace.com>
2018-10-15 11:09:40 +0200

    New Deployment

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ const logger = require('morgan');
 const appsZipkin = require('appmetrics-zipkin');
 
 const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
 
 const app = express();
 
@@ -28,7 +27,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
