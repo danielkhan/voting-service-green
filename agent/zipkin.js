598012b5f19146d8665142fa5ae9ab5146419856
Daniel Khan <daniel.khan@dynatrace.com>
2018-10-19 18:20:45 -0300

    New Deployment

--- agent/zipkin.js
+++ agent/zipkin.js
@@ -6,29 +6,26 @@ const {
 } = require('zipkin');
 
 const zipkinMiddleware = require('zipkin-instrumentation-express').expressMiddleware;
-// const wrapRequest = require('zipkin-instrumentation-request');
-// const request = require('request');
 const CLSContext = require('zipkin-context-cls');
 const localServiceName = 'express-frontend'; 
 
-// const CLSContext = require('zipkin-context-cls');
 const { HttpLogger } = require('zipkin-transport-http');
 
-// Setup the tracer to use http and implicit trace context
-const tracer = new Tracer({
-  ctxImpl: new CLSContext('zipkin'),
-  recorder: new BatchRecorder({
-    logger: new HttpLogger({
-      endpoint: 'http://localhost:9411/api/v2/spans',
-      jsonEncoder: JSON_V2
-    })
-  }),
-  localServiceName,
-});
+
 
 module.exports = (localServiceName) => {
+  const tracer = new Tracer({
+    ctxImpl: new CLSContext('zipkin'),
+    recorder: new BatchRecorder({
+      logger: new HttpLogger({
+        endpoint: 'http://localhost:9411/api/v2/spans',
+        jsonEncoder: JSON_V2
+      })
+    }),
+    localServiceName,
+  });
+  
   return {
     middleware: () => zipkinMiddleware({ tracer }),
-    // request: (remoteServiceName) => wrapRequest(request, { tracer, remoteServiceName }),
   }
 } 
