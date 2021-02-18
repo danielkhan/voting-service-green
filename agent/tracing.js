69f7a8b764aa6b8b1f470ca0361358b75b855557
Daniel Khan <daniel@khan.io>
2021-02-18 01:42:35 +0100

    remove init

--- agent/tracing.js
+++ agent/tracing.js
@@ -12,42 +12,17 @@ const { JaegerExporter } = require("@opentelemetry/exporter-jaeger");
 
 let _serviceName = "";
 
-const provider = new NodeTracerProvider({
-  plugins: {
-    mongodb: {
-      enabled: true,
-      path: "@opentelemetry/plugin-mongodb",
-      enhancedDatabaseReporting: true,
-    },
-    /*
-    mongoose: {
-      enabled: true,
-      path: "@wdalmut/opentelemetry-plugin-mongoose",
-      enhancedDatabaseReporting: true,
-    },
-    */
-    http: {
-      enabled: true,
-      path: "@opentelemetry/plugin-http",
-    },
-    express: {
-      enabled: true,
-      path: "@opentelemetry/plugin-express",
-    },
-  },
-});
-provider.register();
-module.exports.init = (serviceName) => {
-  _serviceName = serviceName;
-  provider.addSpanProcessor(new SimpleSpanProcessor(new ConsoleSpanExporter()));
+const provider = new NodeTracerProvider();
+_serviceName = "service-blue";
+provider.addSpanProcessor(new SimpleSpanProcessor(new ConsoleSpanExporter()));
 
-  // Enable jaeger exporter
-  const options = {
-    serviceName: _serviceName,
-  };
-  const jaegerExporter = new JaegerExporter(options);
-  provider.addSpanProcessor(new SimpleSpanProcessor(jaegerExporter));
+// Enable jaeger exporter
+const options = {
+  serviceName: _serviceName,
 };
+const jaegerExporter = new JaegerExporter(options);
+provider.addSpanProcessor(new SimpleSpanProcessor(jaegerExporter));
+provider.register();
 
 module.exports.getTracer = (name) => {
   return opentelemetry.trace.getTracer(name);
